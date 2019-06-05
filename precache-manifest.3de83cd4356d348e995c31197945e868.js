9bbd06377046518752e186e2b2a54709af0df6c0
Slava <zakharovvu@gmail.com>
2019-06-05 09:13:27 +0300

    Updates

---
+++ precache-manifest.3de83cd4356d348e995c31197945e868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4adfb9fbe9a3e53246b15bc94e572265",
+    "url": "/budgets/index.html"
+  },
+  {
+    "revision": "3ad59c10c31ab64471b6",
+    "url": "/budgets/static/css/main.0a94d41e.chunk.css"
+  },
+  {
+    "revision": "0f39c6670ee6f7304d7e",
+    "url": "/budgets/static/js/2.4ca53546.chunk.js"
+  },
+  {
+    "revision": "3ad59c10c31ab64471b6",
+    "url": "/budgets/static/js/main.446cb610.chunk.js"
+  },
+  {
+    "revision": "1e2d2d4efce4ff871de1",
+    "url": "/budgets/static/js/runtime~main.260f4a1b.js"
+  }
+]);
